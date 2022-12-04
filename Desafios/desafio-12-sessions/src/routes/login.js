50b319d65593c79ecb49ba903717ccc558728b6d
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-12-04 18:00:23 -0300

    update routes

--- Desafios/desafio-12-sessions/src/routes/login.js
+++ Desafios/desafio-12-sessions/src/routes/login.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   if (req.session.username) {
     return res.render("home");
   } else {
-    res.redirect("/login");
+    res.render("login");
   }
 });
 
@@ -16,19 +16,6 @@ router.post("/", async (req, res) => {
   const { user } = req.body;
   req.session.username = user;
   res.redirect("/");
-});
-
-router.get("/login", (req, res) => {
-  const { user } = req.query;
-
-  if (req.session.username) {
-    res.render("home");
-  } else if (user) {
-    req.session.username = user;
-    res.render("home");
-  } else {
-    res.render("login");
-  }
 });
 
 router.get("/logout", checkUserLogged, (req, res) => {
