d76ffc45ca85607845f2b94dbcc9cbf37daab1dd
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-12-11 18:28:21 -0300

    update login route

--- Desafios/desafio-13-autenticacion/src/routes/login.js
+++ Desafios/desafio-13-autenticacion/src/routes/login.js
@@ -1,21 +1,25 @@
 const express = require("express");
-
-const checkUserLogged = require("./../middleware/logged.js");
+const passport = require("passport");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  if (req.session.username) {
+  if (req.isAuthenticated()) {
     res.redirect("/");
   } else {
     res.render("login");
   }
 });
 
-router.post("/", async (req, res) => {
-  const { user } = req.body;
-  req.session.username = user;
-  res.redirect("/");
-});
+router.post(
+  "/",
+  passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/loginError",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 
 module.exports = router;
