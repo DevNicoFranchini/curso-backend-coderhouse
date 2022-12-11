e4764f5b936e52d1e70d6bc2b77979ca3161146d
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-12-11 18:28:27 -0300

    update logout route

--- Desafios/desafio-13-autenticacion/src/routes/logout.js
+++ Desafios/desafio-13-autenticacion/src/routes/logout.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
-const checkUserLogged = require("./../middleware/logged.js");
-
 const router = express.Router();
 
-router.get("/", checkUserLogged, (req, res) => {
-  const username = req.session.username;
-  req.session.destroy();
-  res.render("logout", { username });
+router.get("/", (req, res) => {
+  const username = req.session.passport.user.username;
+  req.logout((err) => {
+    if (err) {
+      console.log(err);
+      return res.send("Hubo un error al cerrar sesion");
+    }
+    req.session.destroy();
+    res.render("logout", { username });
+  });
 });
 
 module.exports = router;
