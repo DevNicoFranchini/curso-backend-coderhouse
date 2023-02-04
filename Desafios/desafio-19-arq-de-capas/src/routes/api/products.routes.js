19a3fd27838eb78aab8e270885713c91d8063b76
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-02-04 15:58:29 -0300

    fixing products route

---
+++ Desafios/desafio-19-arq-de-capas/src/routes/api/products.routes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import faker from 'faker';
+
+const router = express.Router();
+const { datatype, commerce, image } = faker;
+
+faker.locale = 'es';
+
+router.get('/test', (req, res) => {
+	try {
+		let products = [];
+
+		for (let i = 0; i < 5; i++) {
+			products.push({
+				id: datatype.uuid(),
+				title: commerce.productName(),
+				price: commerce.price(),
+				thumbnail: image.image(),
+			});
+		}
+
+		res.render('products', { products: products });
+	} catch (error) {
+		res.status(500).send('Se produjo un error en el servidor :(');
+	}
+});
+
+export { router as ProductsRouter };
