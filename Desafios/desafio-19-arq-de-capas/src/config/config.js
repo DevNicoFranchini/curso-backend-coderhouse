1d17e2c5f7b905cc406bfbdc4a13d9d7ad25539e
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-02-04 15:44:21 -0300

    config changes

--- Desafios/desafio-19-arq-de-capas/src/config/config.js
+++ Desafios/desafio-19-arq-de-capas/src/config/config.js
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
+export const options = {
 	port: process.env.PORT || 8080,
 	mongodb: process.env.MONGO || 'mongodb://localhost:27017',
 	mongosessions: process.env.SESSIONS || 'mongodb://localhost:27017',
