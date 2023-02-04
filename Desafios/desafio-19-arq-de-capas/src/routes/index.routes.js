bab582385aabf1eafb649ce837ec8e27ae1c0db7
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-02-04 15:58:39 -0300

    fixing index routes

--- Desafios/desafio-19-arq-de-capas/src/routes/index.routes.js
+++ Desafios/desafio-19-arq-de-capas/src/routes/index.routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { ProductsRouter } from './api/products.routes';
-import { UserRouter } from './api/user.routes';
+import { ProductsRouter } from './api/products.routes.js';
+import { UserRouter } from './api/user.routes.js';
 
 const router = express.Router();
+
+// GET
+router.get('/', (req, res) => {
+	res.send('Test');
+});
 
 router.use('/users', UserRouter);
 router.use('/products', ProductsRouter);
