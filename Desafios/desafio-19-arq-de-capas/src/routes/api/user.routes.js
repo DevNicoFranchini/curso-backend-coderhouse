550d674ce330f4c5760a98ee378684474f5e3bed
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-02-05 18:37:48 -0300

    updating user routes

--- Desafios/desafio-19-arq-de-capas/src/routes/api/user.routes.js
+++ Desafios/desafio-19-arq-de-capas/src/routes/api/user.routes.js
@@ -1,31 +1,102 @@
 import express from 'express';
 import passport from 'passport';
+import os from 'os';
+
+import {
+	loginStrategy,
+	signupStrategy,
+	serializeUser,
+	deserializeUser,
+} from '../../persistence/passport/passport.js';
+
+loginStrategy();
+signupStrategy();
+serializeUser();
+deserializeUser();
 
 const router = express.Router();
+
+// Home users
+router.get('/', (req, res) => {
+	res.send('Test users');
+});
 
 // Signup
 router.get('/signup', (req, res) => {
 	if (req.isAuthenticated()) {
-		res.redirect('/');
+		res.redirect('/api');
 	} else {
 		res.render('signup');
 	}
 });
 
 router.post(
-	'/',
+	'/signup',
 	passport.authenticate('signup', {
-		successRedirect: '/',
-		failureRedirect: '/signupError',
+		successRedirect: '/api',
+		failureRedirect: '/api/users/signupError',
 	}),
 	(req, res) => {
-		res.redirect('/');
+		res.redirect('/api');
 	}
 );
 
 // Signup error
 router.get('/signupError', (req, res) => {
 	res.render('signupError');
+});
+
+// Login
+router.get('/login', (req, res) => {
+	if (req.isAuthenticated()) {
+		res.redirect('/api');
+	} else {
+		res.render('login');
+	}
+});
+
+router.post(
+	'/login',
+	passport.authenticate('login', {
+		successRedirect: '../',
+		failureRedirect: '/api/users/loginError',
+	}),
+	(req, res) => {
+		res.redirect('/api');
+	}
+);
+
+// Login error
+router.get('/loginError', (req, res) => {
+	res.render('loginError');
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+	const username = req.session.passport.user.username;
+	req.logout((err) => {
+		if (err) {
+			console.log(err);
+			return res.send('Hubo un error al cerrar sesión');
+		}
+		req.session.destroy();
+		res.render('logout', { username });
+	});
+});
+
+// Info
+const info = {
+	SO: process.platform,
+	Node: process.version,
+	RSS: JSON.stringify(process.memoryUsage(), null, '\t'),
+	Path: process.execPath,
+	Id: process.pid,
+	Folder: process.cwd(),
+	Process: os.cpus().length,
+};
+
+router.get('/info', (req, res) => {
+	res.render('info', { info });
 });
 
 export { router as UserRouter };
