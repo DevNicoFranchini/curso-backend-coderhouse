6d10a97743a5ebfe3baee0dd67fa938a193d723b
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-02-11 13:29:30 -0300

    update config

--- Desafios/desafio-20-arq/src/config/config.js
+++ Desafios/desafio-20-arq/src/config/config.js
@@ -1,9 +1,28 @@
 import * as dotenv from 'dotenv';
+import ParsedArgs from 'minimist';
 
 dotenv.config();
 
+const objArgs = ParsedArgs(process.argv.slice(2), {
+	alias: {
+		p: 'port',
+		m: 'mode',
+		e: 'env',
+	},
+	default: {
+		port: process.env.PORT || 8080,
+		mode: 'FORK',
+		env: 'DEV',
+	},
+});
+
 export const options = {
-	port: process.env.PORT || 8080,
+	server: {
+		port: objArgs.port,
+		mode: objArgs.mode,
+		node_env: objArgs.env,
+		dbType: process.env.DB_TYPE || 'MONGO',
+	},
 	mongodb: process.env.MONGO || 'mongodb://localhost:27017',
 	mongosessions: process.env.SESSIONS || 'mongodb://localhost:27017',
 };
